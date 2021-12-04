fca507c2d927de77bcf406a1b682044d5c0ac6de
AndriiPets <nnggtrgftgd@gmail.com>
2021-12-04 05:17:54 +0200

    Homework
    added views user, todoitems
    added corresponding urls

--- todo/todo_app/urls.py
+++ todo/todo_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-import views
+from . import views
 
 urlpatterns = [
-    path('tags/', views.get_tags, name='get_tags')
+    path('tags/', views.get_tags, name='get_tags'),
+    path('todoitems/', views.get_todo_items, name='get_todoitems'),
+    path('users/', views.get_users, name='get_users')
 ]
--- todo/todo_app/views.py
+++ todo/todo_app/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
-from models import Tag
+from . import models
 
 
 def get_tags(request: HttpRequest):
-    tags = Tag.objects.get()
+    tags = models.Tag.objects.all()
     context = {'tags': tags, 'title': "Tags List"}
     return render(request, 'todo_app/tags.html', context=context)
+
+
+def get_todo_items(request: HttpRequest):
+    todo_item = models.TodoItem.objects.all()
+    context = {'todo_item': todo_item, 'title': "Todo_items List"}
+    return render(request, 'todo_app/todo_items.html', context=context)
+
+
+def get_users(request: HttpRequest):
+    user = models.User.objects.all()
+    context = {'user': user, 'title': "Users List"}
+    return render(request, 'todo_app/users.html', context=context)
+
