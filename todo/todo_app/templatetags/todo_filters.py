cefd411472c5e258e528aa8f200cd86f68e2441d
Maksym Maksymchuk <twister.maxim@gmail.com>
2022-01-23 13:25:22 +0200

    Commit 006
    Add simple_tag to count account unacknowledged messages in navbar.html

---
+++ todo/todo_app/templatetags/todo_filters.py
@@ -0,0 +1,11 @@
+from django import template
+
+from todo_app.models import Message
+
+register = template.Library()
+
+
+@register.filter
+def get_unread_messages_num(account_messages: Message.objects):
+    """Get an amount of unacknowledged messages"""
+    return len(account_messages.filter(acknowledged=False))
